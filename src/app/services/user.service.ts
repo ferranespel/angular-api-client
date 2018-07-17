b2d2f8bc2be3886cd562ad64c883c5486e545baa
ferransson <ferransson@gmail.com>
2018-07-17 01:03:50 +0000

    Login access included, need improvement for localStorage of identity and token

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -8,6 +8,8 @@ import {User} from '../models/user';
 export class UserService {
 
 	public url: string;
+	public identity;
+	public token;
 	
 	constructor(
 		public _http: HttpClient
@@ -25,6 +27,37 @@ export class UserService {
 		let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
 
 		return this._http.post(this.url+'register', params, {headers: headers});
+	}
+
+	signup(user, gettoken = null): Observable<any>{
+		if(gettoken != null){
+			user.gettoken = 'true';
+		}
+		let json = JSON.stringify(user);
+		let params = 'json='+json;
+
+		let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+
+		return this._http.post(this.url+'login', params, {headers: headers});
+	}
+
+	getIdentity(){
+		let identity = JSON.parse(localStorage.getItem('identity'));
+		if(identity != "undefined"){
+			this.identity = identity;
+		}else{
+			this.identity = null;
+		}
+		return this.identity;
+	}
+	getToken(){
+		let token = JSON.parse(localStorage.getItem('token'));
+		if(token != "undefined"){
+			this.token = token;
+		}else{
+			this.token = null;
+		}
+		return this.token;
 	}
 
 }
